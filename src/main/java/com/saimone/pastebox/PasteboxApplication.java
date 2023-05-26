91323d4c500a57b4881633bdd8c3506338a75579
Andriy Khrushch <csgo.reserv@gmail.com>
2023-05-26 11:05:09 +0300

    init

---
+++ src/main/java/com/saimone/pastebox/PasteboxApplication.java
@@ -0,0 +1,13 @@
+package com.saimone.pastebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saimone/pastebox/PasteboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saimone.pastebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
